c1aed648af06fa514a2d6592f16a329a5f4a98f8
zhuqianye <q835041058@gmail.com>
2017-07-05 15:48:17 +0800

    error

--- webpack_and_react/webpack.config.js
+++ webpack_and_react/webpack.config.js
@@ -18,16 +18,3 @@ module.exports={
 		// publicPath:"/"
 	}
 }
-// module.exports={
-// 	entry:path.resolve(__dirname,'./app/index.js'),
-// 	// __dirname表示当前运行的js所在的目录
-// 	output:{
-// 		path:path.resolve(__dirname,'/build'),
-// 		filename:"bundle.js"
-// 	},
-// 	devServer:{
-// 		port:8080,
-// 		contentBase:"./build",
-// 		historyApiFallback:true
-// 	}
-// }
